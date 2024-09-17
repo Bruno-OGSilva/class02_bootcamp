9fa853319229163333215d7072d5de17e87f3e13
Bruno Silva <bruno.ogs@gmail.com>
2024-09-16 22:25:33 -0400

    adding exercise 10

--- exercises.py
+++ exercises.py
@@ -4,10 +4,10 @@
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
-num1 = int(input("Inserir um numero inteiro: "))
-num2 = int(input("Inserir outro numero inteiro: "))
-resultado = num1 // num2
-print(resultado)
+#num1 = int(input("Inserir um numero inteiro: "))
+#num2 = int(input("Inserir outro numero inteiro: "))
+#resultado = num1 // num2
+#print(resultado)
 
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
@@ -18,6 +18,10 @@ print(resultado)
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+import math
+raio_do_circulo = float(input("Digite o raio: "))
+area_do_circulo = math.pi * raio_do_circulo ** 2
+print(f"{area_do_circulo:.2f}")
 
 # #### Strings (`str`)
 
