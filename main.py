38eca4d42ad789f14cf399b130b8af35268247a3
Bruno Silva <bruno.ogs@gmail.com>
2024-09-16 23:03:58 -0400

    adding the main

---
+++ main.py
@@ -0,0 +1,16 @@
+
+# try:
+#     resultado = len(3)
+#     print(resultado)
+# except TypeError as e:
+#     print(e)
+# else:
+#     print("tudo ocorreu bem")
+# finally:
+#     print("o importante e participar")
+
+numero = 10
+if isinstance(numero, int):
+    print("A variável é um inteiro.")
+else:
+    print("A variável não é um inteiro.")
